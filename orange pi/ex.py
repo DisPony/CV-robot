552666ff88a66a69039b5e958cc14b6dcf5fd0da
DisPony <nikitaklimenko2000@gmail.com>
2018-12-01 20:01:12 +0300

    Program for testing

--- orange pi/ex.py
+++ orange pi/ex.py
@@ -1,5 +1,26 @@
 import serial
 
-image = cv2.imread("1.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite("2.jpg", gray)
+BAUDRATE = 9600
+PORT = "/dev/ttyUSB0"
+
+
+arduino = serial.Serial(PORT, BAUDRATE)
+
+
+def get_command():
+    b = bytearray(9)
+    b[0] = int(input("Function number: "))
+    b[1] = min(255, int(input("First arg: ")))
+    b[2] = min(255, int(input("Second arg arg: ")))
+
+    return b
+
+
+while True:
+    command = get_command()
+    arduino.write(command)
+
+#
+# for byte in get_command():
+#     print byte
+
