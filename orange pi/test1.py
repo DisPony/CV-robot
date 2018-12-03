0aca8c4e72245a2e10775bf3051d14b062626890
DisPony <nikitaklimenko2000@gmail.com>
2018-12-03 22:57:45 +0300

    Update test1.py

--- orange pi/test1.py
+++ orange pi/test1.py
@@ -9,4 +9,13 @@ import Robot
 ser = serial.Serial(PORT, BAUDRATE)
 myRobot = Robot.Robot(ser)
 
-
+while True:
+    ch = input("command: ")
+    if(ch == "w"):
+        myRobot.move(500)
+    elif(ch == "s"):
+        myRobot.move(-500)
+    elif(ch == "d"):
+        myRobot.turn(500)
+    elif(ch == "a"):
+        myRobot.turn(-500)
