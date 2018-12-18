6980a9f63de9e286c80229f89936c00edf56dae5
DisPony <nikitaklimenko2000@gmail.com>
2018-12-18 17:43:01 +0300

    Update camera_test.py

--- orange pi/camera_test.py
+++ orange pi/camera_test.py
@@ -1,7 +1,28 @@
+import numpy as np
 import cv2
 
-# cv2.imshow(" ", cv2.imread("1.jpg"))
-# cv2.waitKey()
+cap = cv2.VideoCapture(0)
 
-image = cv2.imread("1.png")
-cv2.imwrite("hello.png", image)
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret==True:
+        frame = cv2.flip(frame,0)
+
+        # write the flipped frame
+        out.write(frame)
+
+        cv2.imshow('frame',frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
+
