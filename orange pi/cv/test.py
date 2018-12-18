fca79edece8171c8694b6f7e78ce89f57b50a2ea
DisPony <nikitaklimenko2000@gmail.com>
2018-12-18 17:04:12 +0300

    Update test.py

--- orange pi/cv/test.py
+++ orange pi/cv/test.py
@@ -5,7 +5,7 @@ import numpy as np
 import argparse
 import imutils
 import cv2
- 
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--images", required=True, help="path to images directory")
 args = vars(ap.parse_args())
@@ -15,33 +15,33 @@ print(list(paths.list_images(args["images"])))
 for imagePath in paths.list_images(args["images"]):
     # load the image and resize it to (1) reduce detection time
     # and (2) improve detection accuracy
-	image = cv2.imread(imagePath)
-	image = imutils.resize(image, width=min(400, image.shape[1]))
+    image = cv2.imread(imagePath)
+    image = imutils.resize(image, width=min(400, image.shape[1]))
 
-	orig = image.copy()
- 
+    orig = image.copy()
+
     # detect people in the image
-	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
-		padding=(8, 8), scale=1.05)
- 
+    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+                                            padding=(8, 8), scale=1.05)
+
     # draw the original bounding boxes
-	for (x, y, w, h) in rects:
-		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
- 
+    for (x, y, w, h) in rects:
+        cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
     # apply non-maxima suppression to the bounding boxes using a
     # fairly large overlap threshold to try to maintain overlapping
     # boxes that are still people
-	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
- 
+    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
     # draw the final bounding boxes
-	for (xA, yA, xB, yB) in pick:
-		print(xA, yA, xB, yB)
-		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
- 
+    for (xA, yA, xB, yB) in pick:
+        print(xA, yA, xB, yB)
+        cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
     # show some information on the number of bounding boxes
-	filename = imagePath[imagePath.rfind("/") + 1:]
-	print("[INFO] {}: {} original boxes, {} after suppression".format(filename, len(rects), len(pick)))
+    filename = imagePath[imagePath.rfind("/") + 1:]
+    print("[INFO] {}: {} original boxes, {} after suppression".format(filename, len(rects), len(pick)))
     # show the output images
-	cv2.imshow("After NMS", image)
-	cv2.waitKey(0)
+    cv2.imwrite("hello.png", image)
+    cv2.waitKey(0)
