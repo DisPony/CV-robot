47c8ecc131be04098466c36f2bd1c4e464d5e330
DisPony <nikitaklimenko2000@gmail.com>
2018-12-18 18:10:04 +0300

    Update camera_test.py

--- orange pi/camera_test.py
+++ orange pi/camera_test.py
@@ -11,10 +11,9 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 while (cap.isOpened()):
     ret, frame = cap.read()
-    if ret == True:
+    if ret:
         # frame = cv2.flip(frame, 0)
-        (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4),
-                                                padding=(8, 8), scale=1.05)
+        (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
         for (x, y, w, h) in rects:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
         # write the flipped frame
