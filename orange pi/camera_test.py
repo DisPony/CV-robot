9732f198c031a634f50640dfba9d796f02ca6211
DisPony <nikitaklimenko2000@gmail.com>
2018-12-18 18:07:09 +0300

    Update camera_test.py

--- orange pi/camera_test.py
+++ orange pi/camera_test.py
@@ -5,13 +5,18 @@ cap = cv2.VideoCapture(0)
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.cv.FOURCC(*'XVID')
-out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (640, 480))
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-while(cap.isOpened()):
+while (cap.isOpened()):
     ret, frame = cap.read()
-    if ret==True:
-        frame = cv2.flip(frame,0)
-
+    if ret == True:
+        # frame = cv2.flip(frame, 0)
+        (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4),
+                                                padding=(8, 8), scale=1.05)
+        for (x, y, w, h) in rects:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
         # write the flipped frame
         out.write(frame)
 
@@ -25,4 +30,3 @@ while(cap.isOpened()):
 cap.release()
 out.release()
 cv2.destroyAllWindows()
-
