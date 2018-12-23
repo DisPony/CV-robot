1f182e22330866a3c67631e8629f606aa2472e4d
DisPony <nikitaklimenko2000@gmail.com>
2018-12-23 19:14:30 +0300

    Update Servo.ino

--- arduino/Servo/Servo.ino
+++ arduino/Servo/Servo.ino
@@ -203,7 +203,7 @@ int turnRobotParallel(float angle){
 */
 //------------------------------------------------------------------------//
 
-//#define MOVETEST
+#define MOVETEST
 //#define ROBOTTURNTEST
 //#define SETPOSITIONTEST
 //#define SERIALTEST
